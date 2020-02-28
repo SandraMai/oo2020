121dd1e1d075634cbd1497b94052016b2da35b1d
Sandra Maidla <55189740+SandraMai@users.noreply.github.com>
2020-02-28 10:31:05 +0200

    Update Proov.java

--- w03_klassikomplekt/Proov.java
+++ w03_klassikomplekt/Proov.java
@@ -4,10 +4,12 @@ public class Proov{
         Computer sh1 = new Computer(sp1);
         System.out.println(sp1.getAmount());
         System.out.println(sh1.addAmount(2));
+        System.out.println(sh1.finalAmount);
         System.out.println(sh1.specs());
     }
 }
 
 //3
+//5
 //5
 //(HP, 8gb ram, 500gb, 469.99eur)
